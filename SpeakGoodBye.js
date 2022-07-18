ba38ecc05eedcd505baac21c78b08a00c7b3fb1a
Hazy2028 <62305718+Hazy2028@users.noreply.github.com>
2022-07-18 15:04:35 +0200

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,9 @@
+
+
+
+var byeSpeaker = {};
+byeSpeaker.name = "Bye";
+var speakWord = "Good Bye";
+byeSpeaker.saybye =  function speak(name) {
+  console.log(speakWord + " " + byeSpeaker.name);
+}
---
+++ SpeakHello.js
@@ -0,0 +1,12 @@
+
+
+
+
+  var helloSpeaker = {};
+  helloSpeaker.name = "Hi";
+  var speakWord = "Hello ";
+  helloSpeaker.sayHello = function speak(name) {
+  console.log(speakWord + " " + helloSpeaker.name);
+}
+
+
